1e50c6e606463eeb954fe725528b7b4e435780f3
Jean Arnhold <jeanarnhold939@gmail.com>
2025-05-16 20:13:20 -0300

    up

--- AULA13-NODE/basic.js
+++ AULA13-NODE/basic.js
@@ -1,11 +1,30 @@
 const http = require('http');
+const { JSON } = require('stream/consumers');
 
 const server = http.createServer( (req, res) => {
 
+   let cidade = {
+      nome: 'Feliz',
+      estado: 'RS',
+      pais: 'Brasil',
+      populacao: 10000,
+      pontosTuristicos: [
+         'Praça Central',
+         'Igreja Matriz',
+         'Parque Natural'
+      ],
+      clima: 'Temperado',
+      temperatura: {
+         min: 10,
+         max: 25
+      }
+
+   }
+   
 
    // console.log(req.url, req.method, req.headers);
 
-   res.write('Ola, dentro do HTML');
+   res.write( JSON.stringify(cidade) );
    res.end();
 })
 
